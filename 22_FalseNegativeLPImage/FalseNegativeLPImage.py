283270dcfaaca805c6324af21f6df6b17d32966e
JM_Sa <srsten1@gmail.com>
2022-11-17 17:59:50 +0900

    22_ : 콘솔창에서 진행률 확인할 수 있도록 수정

--- 22_FalseNegativeLPImage/FalseNegativeLPImage.py
+++ 22_FalseNegativeLPImage/FalseNegativeLPImage.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+print("Start ... ")
+
 fPATH = os.getcwd()+"\\"
 Ext = [".jpg", ".bmp", ".png"] 
 
@@ -10,11 +12,16 @@ if not(os.path.isdir(dPATH)):
     os.makedirs(os.path.join(dPATH))
 
 FileList = os.listdir()
-
+ii = 0
 for _File in FileList:
+    print(str(ii) + " / " + str(len(FileList)), end = '\r')
     vFmt = _File[-4:]
     if any(vFmt == _ext for _ext in Ext):
         vFileName = _File[:-4]
         if vFileName[-1] == '_':
             shutil.copy(_File, dPATH)
             os.remove(_File)
+    ii += 1
+
+print("")
+print("End ... ")
