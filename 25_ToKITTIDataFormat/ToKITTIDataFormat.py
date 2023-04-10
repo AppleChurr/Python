1b66437109d1ad6faf43313d5dc11e16bab7e153
JM_Sa <srsten1@gmail.com>
2023-04-10 16:25:00 +0900

    ini를 통해 몇몇 값 조정 할 수 있게 수정
    높이, 너비 or로 특정 값 이하 필터링
    시작 번호 및 10자리 정수형 제목 표기
    특정 해상도로 이미지 변환 및 폴더에 저장

--- 25_ToKITTIDataFormat/ToKITTIDataFormat.py
+++ 25_ToKITTIDataFormat/ToKITTIDataFormat.py
@@ -5,9 +5,19 @@ import numpy as np
 import time
 import glob
 
-SAVEPATH = "./KITTI/"
+SAVEPATH = "./KITTI_"
 DATAEXT = ".txt"
 IMGEXT = ".jpg"
+
+config = open("config.ini", 'r')
+
+ditcConfig = {"START_INDEX":0, "MIN_WIDTH":30, "MIN_HEIGHT":30, "DIST_IMG_WIDTH":1280, "DIST_IMG_HEIGHT":720}
+for line in config.readlines():
+    line = line.split('\n')[0]
+    dictEle = line.split('=')
+    ditcConfig.update({dictEle[0]:int(dictEle[1])})
+
+SAVEPATH += str(ditcConfig["DIST_IMG_WIDTH"]) + "x" + str(ditcConfig["DIST_IMG_HEIGHT"]) + "/"
 
 if not(os.path.isdir(SAVEPATH)):
     os.makedirs(os.path.join(SAVEPATH))
@@ -15,6 +25,10 @@ if not(os.path.isdir(SAVEPATH)):
 DirFileList = os.listdir()
 
 names = open("name.names", 'r')
+
+
+
+save_idx = ditcConfig["START_INDEX"]
 
 lClass = []
 
@@ -24,18 +38,24 @@ for line in lines:
     lClass.append(line[0])
 names.close()
 
+iw = 1280
+ih = 720
+
 for file_ in DirFileList:
     if(file_[-4:] == DATAEXT):
+        title = file_[:-4]
         print(file_)
         lData = open(file_, 'r')
 
-        if os.path.isfile(file_[:-4] + IMGEXT):
-            with Image.open(file_[:-4] + IMGEXT) as img:
-                iw, ih = img.size
+        if os.path.isfile(title + IMGEXT):
+            with Image.open(title + IMGEXT) as img:
+                # iw, ih = img.size
                 print(f"Image size: {iw}x{ih}")
+                img = img.resize([ditcConfig["DIST_IMG_WIDTH"], ditcConfig["DIST_IMG_HEIGHT"]])
+                img.save(SAVEPATH + str(save_idx).zfill(10) + IMGEXT)
             
 
-            kittiData = open(SAVEPATH + file_, 'w')
+            kittiData = open(SAVEPATH + str(save_idx).zfill(10) + DATAEXT, 'w')
 
             labels = lData.readlines()
             for label in labels:
@@ -46,8 +66,13 @@ for file_ in DirFileList:
                 width = float(label[3]) * iw
                 height = float(label[4]) * ih
 
+                if((width <= ditcConfig["MIN_WIDTH"]) or (height <= ditcConfig["MIN_HEIGHT"])):
+                    continue
+
                 left = float(label[1]) * iw - width / 2
                 top = float(label[2]) * ih - height / 2
+
+                
 
                 kittiLabel = [lClass[int(label[0])], "0.00", "0", "0.00", str(left), str(top), str(left + width), str(top + height), "0.00", "0.00", "0.00", "0.00", "0.00", "0.00", "0.00"]
 
@@ -60,6 +85,8 @@ for file_ in DirFileList:
                 kittiData.write('\n')
 
             kittiData.close()
+
+            save_idx += 1
             
 
 
